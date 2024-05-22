bf1f5cfe3774eceaadd99f0f0d7cb08abca7c6a5
tUNGlEon00 <tungleon24@gmail.com>
2024-05-22 09:45:54 +0700

    LTT832_Cau2

--- BaiKiemTra/LTT832Person.java
+++ BaiKiemTra/LTT832Person.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class LTT832Person {
     public String Hoten;
     public int maSv;
     public int Tuoi; 
+
+    Scanner sc = new Scanner(System.in);
+
+    public void nhap()
+    {
+        System.out.println(" nhap thong tin: ");
+        System.out.println(" nhap ho va ten: ");
+        Hoten = sc.nextLine();
+        System.out.println(" nhap ma sinh vien: ");
+        maSv = sc.nextInt();
+        System.out.println(" nhap tuoi: ");
+        Tuoi = sc.nextInt();
+    }
+
+    public void xuat()
+    {
+        System.out.println(" thong tin cua ban la: Ho va ten: " + Hoten + " /ntuoi: " + Tuoi + " / ma sinh vien: " + maSv);
+    }
 }
